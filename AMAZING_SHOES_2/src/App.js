bdda51ace3739d47f9f57df0d91281bef6f7711e
reba-1984 <reba_1984@hotmail.com>
2021-10-11 16:49:11 -0500

    cambios

--- AMAZING_SHOES_2/src/App.js
+++ AMAZING_SHOES_2/src/App.js
@@ -3,21 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App" background="fondo.jpg">
+      <header>
+        <img src="logo_trans2.png" alt="Logotipo" class="logo_nav" />
+        <nav class="container">
+          <ul>
+            <li><a href="login.html">Logearse</a></li>
+            <li><a href="#">Sobre Nosotros</a></li>
+            <li><a href="#">Contacto</a></li>
+          </ul>
+        </nav>
       </header>
+
+
+      <footer class="piedepagina">
+        <div>Todos los derechos reservados | Misión-Tic 2022 | THE-FIVE-GEEKS</div>
+      </footer>
     </div>
   );
 }
